d1249e154818daf03024776d97066fb3441be52a
FenyvesiPeti <164069030+FenyvesiPeti@users.noreply.github.com>
2024-09-25 12:19:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"Ha nem voltatok órán / elfelejtettéek hohgy kell beállítani az xampp-ot itt egy videó:";
+    ?>
+</body>
+</html>
