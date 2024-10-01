4b97ff5e42f06e4b931cad2b3d22bd5a3a0fad24
FenyvesiPeti <164069030+FenyvesiPeti@users.noreply.github.com>
2024-10-01 22:47:26 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer class="site-footer container-fluid">
     <div class="row">
-        <p class="col-4 text-start">Készítette: Fenyvesi Péter, Bakri Ferenc, Fehér Attila</p>
-        <p class="col-4">&copy; 2024 Kisállat örökbefogadó</p>
-        <p class="col-4 text-end"><a href="#top">Vissza a tetejére</a></p>
+        <p class="col-8 text-center mb-0 ps-20">Copyright &copy; 2024 Kisállat örökbefogadó. Minden jog fenntartva</p>
+        <p class="col-4 text-start">Készítette: <br>Fenyvesi Péter, Bakri Ferenc, Fehér Attila</p>
     </div>
 </footer>
